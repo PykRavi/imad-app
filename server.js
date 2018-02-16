cdb8acc2e1cc4a369d0e8bdeeff27a916d52e8a2
PykRavi <32331779+PykRavi@users.noreply.github.com>
2018-02-16 22:43:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function(req,res){
  //  res.send('article one will be served soon') 
- res.sendFile(path.join(__dirname,'ui','article-one.html'));
+ res.sendFile(path.join(__dirname,'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
