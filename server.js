4db976c660ae0db8892cfbec7d190dbb47de4d7d
PykRavi <32331779+PykRavi@users.noreply.github.com>
2018-02-25 12:21:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/article-two',function(req,res){
 }
 );
 
-app.get('article-three',function(req,res){
+app.get('/article-three',function(req,res){
     res.send('article three will be server here')
 });
 
