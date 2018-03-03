dc71d0c61e1e77d24d9ea669123941048508e184
PykRavi <32331779+PykRavi@users.noreply.github.com>
2018-03-03 19:36:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ button.onclick = function (){
     request.onreadystatechange = function () {
         if ( request.readystate === XMLHttpRequest.DONE ) 
         {
-            if ( request.status == 200 ) {
+            if ( request.status === 200 ) {
                 var cntr = request.responseText;
                 console.log('came here');
                 var span = document.getElementById('counter');
