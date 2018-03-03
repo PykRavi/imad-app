9a336a7914d028b5e9288288b51eba3926f2512e
PykRavi <32331779+PykRavi@users.noreply.github.com>
2018-03-03 18:14:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+var button = getElementByid('button');
+button.onclick = function (){
+    
+};
+var img = getElementByid('getimg');
+function MoveLeft()
+{
+    marginLeft=marginLeft -1;
+    img.style.MarginLeft =marginLeft + "px";
+}
+img.onclick = function(){
+    var interval = Setinterval(MoveLeft,50);
+};
