d334c1c244464fc51570a384214f0f00862d40b0
PykRavi <32331779+PykRavi@users.noreply.github.com>
2018-03-03 18:20:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/main.js',function(req,res){
+    res.sendfile(Path.join(__dirname,'ui','main.js'));
+});
 counter =0;
 app.get('/counter',function(req,res){
    counter = counter + 1;
