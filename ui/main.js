543315374cf1925e52b5dc4587f92920e27f0222
PykRavi <32331779+PykRavi@users.noreply.github.com>
2018-03-03 19:08:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,20 @@ var button = document.getElementById('button');
 button.onclick = function (){
     var request = new XMLHttpRequest();
     
+    request.onreadystatechange = function () {
+        if ( request.readystate === XMLHttpRequest.DONE ) 
+        {
+            if ( request.status == 200 ) {
+                var counter = request.responseText;
+                document.getElementById('counter').InnerHTML = Counter.toString();
+            }
+        }
+        
+    };
     
-    document.getElementById('counter').InnerHTML = counter;
+    request.open('GET','//http://pykravi05.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
     
     
 };
